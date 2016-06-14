8750a100584f6a704fba9143556d93ab70b637a1
MilosMisic <mybadmc@hotmail.com>
2016-06-15 01:04:00 +0200

    worked on Connect.java

--- src/main/Connect.java
+++ src/main/Connect.java
@@ -13,8 +13,6 @@ public class Connect {
 		try {
 			URL url = new URL(
 					"http://api.openweathermap.org/data/2.5/weather?q=Belgrade&APPID=3b533332460a1f75c7b0cedb5ed99517");
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 
