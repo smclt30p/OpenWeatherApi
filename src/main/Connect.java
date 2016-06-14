d54e93a577d7a934cd2b5cb19fba11e496247f11
Ognjen Galic <smclt30p@gmail.com>
2016-06-15 01:35:11 +0200

    JSON Responses are 1 line long, no need to read in a while loop

--- src/main/Connect.java
+++ src/main/Connect.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
-public class Connect {
-	public static String start() {
+class Connect {
+	static String start() {
 		try {
 			URL url = new URL(
 					"http://api.openweathermap.org/data/2.5/weather?q=Belgrade&APPID=3b533332460a1f75c7b0cedb5ed99517");
@@ -18,19 +17,14 @@ public class Connect {
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 
-			String response = "";
-			String jsonData = "";
+			return br.readLine();
 
-			while (null != (response = br.readLine())) {
-				jsonData += response;
-			}
-			return jsonData;
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (ProtocolException e) {
+			/* Should never happen */
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("I/O Error: " + e.getMessage());
+			System.exit(1);
 		}
 		return "";
 	}
