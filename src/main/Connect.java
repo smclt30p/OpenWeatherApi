6557decb87773e98cd516d219fe27fcdd24c04cd
Ognjen Galic <smclt30p@gmail.com>
2016-06-17 11:24:32 +0200

    Handle HTTP Errors

--- src/main/Connect.java
+++ src/main/Connect.java
@@ -8,16 +8,30 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 class Connect {
+
+	private static final String APPID = "3b533332460a1f75c7b0cedb5ed99517";
+	private static final String ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
+	private static final String UNITS = "&units=metric";
+
 	static String start() {
+
 		try {
-			URL url = new URL(
-					"http://api.openweathermap.org/data/2.5/weather?q=Belgrade&APPID=3b533332460a1f75c7b0cedb5ed99517");
+
+			URL url = new URL(ENDPOINT + "?q=Belgrade&APPID=" + APPID + UNITS);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
+			int response = con.getResponseCode();
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-
-			return br.readLine();
+			switch (response) {
+				case HttpURLConnection.HTTP_OK:
+					BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					return in.readLine();
+				case HttpURLConnection.HTTP_UNAUTHORIZED:
+					throw new IOException("Invalid API key!");
+				case HttpURLConnection.HTTP_NOT_FOUND:
+					throw new IOException("Open Weather Map is not available!");
+				default:
+					throw new IOException("Invalid HTTP Response!: " + response);
+			}
 
 		} catch (MalformedURLException e) {
 			/* Should never happen */
@@ -26,8 +40,6 @@ class Connect {
 			System.err.println("I/O Error: " + e.getMessage());
 			System.exit(1);
 		}
-
 		throw new RuntimeException("Method fallthrough!");
-
 	}
 }
